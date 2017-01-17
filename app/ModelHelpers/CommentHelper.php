d8d175ace1e9d52f45119b28f481ca0a1268b160
nosun <nosun2008@126.com>
2017-01-17 18:43:54 +0800

    fix bug

--- app/ModelHelpers/CommentHelper.php
+++ app/ModelHelpers/CommentHelper.php
@@ -7,11 +7,20 @@ use DB;
 class CommentHelper{
 
     public static function updateCommentStatus($posts,$status){
-        DB::table(config('cwzg.edbPrefix').'enewspl_1')->whereIn('post_id',$posts)->update(['checked' => $status]);
+        if(count($posts) > 1){
+            DB::table(config('cwzg.edbPrefix').'enewspl_1')->whereIn('post_id',$posts)->update(['checked' => $status]);
+        }else{
+            DB::table(config('cwzg.edbPrefix').'enewspl_1')->where('post_id',$posts)->update(['checked' => $status]);
+        }
+
     }
 
     public static function deleteComments($posts){
-        DB::table(config('cwzg.edbPrefix').'enewspl_1')->whereIn('post_id',$posts)->delete();
+        if(count($posts) > 1){
+            DB::table(config('cwzg.edbPrefix').'enewspl_1')->whereIn('post_id',$posts)->delete();
+        }else{
+            DB::table(config('cwzg.edbPrefix').'enewspl_1')->where('post_id',$posts)->delete();
+        }
     }
 
 }
