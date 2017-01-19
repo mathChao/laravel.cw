8dbc25432d438fa4ae73a77a0b77f78271c8541e
mathChao <math_chao@163.com>
2017-01-19 11:36:17 +0800

    add modify

--- app/Console/Commands/MigrationNews.php
+++ app/Console/Commands/MigrationNews.php
@@ -282,6 +282,6 @@ class MigrationNews extends Command
     }
 
     private function imgPathTransfer($sitePrefix, $str){
-        return str_replace('http://www.cwzg.cn/uploadfile', 'http://static.cwzg.webbig.cn/p', $str);
+        return str_replace('http://www.cwzg.cn/uploadfile', 'http://static.cwzg.cn/p', $str);
     }
 }
